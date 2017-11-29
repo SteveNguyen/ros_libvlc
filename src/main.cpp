fc8b99ce1f2302a649bd1389593efd597869c534
Daniel Reuter <daniel.robin.reuter@googlemail.com>
2017-11-29 02:52:42 +0100

    Some cleanup and reformatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include "ros/ros.h"
 #include "ros_libvlc/RosLibVlc.h"
 
-
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
   ros::init(argc, argv, "libvlc_wrapper");
   ros::NodeHandle nh("~");
-  
-  RosLibVlc* rlv = RosLibVlc::getInstance(nh);
-  
+
+  RosLibVlc *rlv = RosLibVlc::getInstance(nh);
+
   ROS_INFO("Ready to go...");
-  
+
   rlv->spin();
 }
